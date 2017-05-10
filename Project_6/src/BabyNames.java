b40bed816fafe26a0037fcd8f2e2b4937b91af13
Yap <ROS_wuyap@seattleschools.org>
2017-05-10 09:21:17 -0700

    project 6

--- Project_6/src/BabyNames.java
+++ Project_6/src/BabyNames.java
@@ -2,19 +2,29 @@ import java.awt.*;
 import java.io.*;
 import java.util.*;
 
+/**
+ * BabyNames.java
+ * Assignment: Baby Names
+ * Purpose: This assignment practices the use of Scanners and reading from files.
+ *          In addition, the use of DrawingPanel is also employed along with
+ *          concepts of structural decomposition.
+ *
+ * @version 5/3/2017
+ */
+
 public class BabyNames {
-    private static final int SCREEN_WIDTH = 780;
-    private static final int SCREEN_HEIGHT = 560;
+
+    private static final int STARTING_YEAR = 1890;
     private static final int LEGEND_HEIGHT = 30;
     private static final int DECADES = 13;
-    private static final int STARTING_YEAR = 1890;
-    private static final int DECADE_WIDTH = 60;
+    private static final int DECADE_WIDTH = LEGEND_HEIGHT * 2;
+    private static final int SCREEN_WIDTH = 780;
+    private static final int SCREEN_HEIGHT = 500 + 2 * LEGEND_HEIGHT;
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner console = new Scanner(System.in);
         Scanner namesFile = new Scanner(new File("names.txt"));
         Scanner meaningsFile = new Scanner(new File("meanings.txt"));
-        Scanner lineReader = new Scanner("");
 
         introduction();
         String inputName = getName(console);
@@ -30,7 +40,7 @@ public class BabyNames {
             DrawingPanel panel = new DrawingPanel(SCREEN_WIDTH, SCREEN_HEIGHT);
             Graphics pen = panel.getGraphics();
             drawGraph(pen);
-            drawData(lineReader, pen, inputName, namePopularity, nameMeaning);
+            drawData(pen, inputName, namePopularity, nameMeaning);
         }
     }
 
@@ -39,12 +49,13 @@ public class BabyNames {
         System.out.println("This program allows you to search through");
         System.out.println("data from the Social Security Administration");
         System.out.println("to see how popular a name has been since 1980.");
+        System.out.println();
     }
 
     private static String getName(Scanner console) {
         // Receives and formats user's name.
         System.out.print("Name: ");
-        return console.next().toLowerCase();
+        return console.next();
     }
 
     private static String getFileData(Scanner file, String inputName) {
@@ -68,16 +79,16 @@ public class BabyNames {
         // Decade intervals.
         pen.setColor(Color.BLACK);
         for (int i = 0; i < DECADES; i++) {
-            pen.drawString("" + (STARTING_YEAR + 10 * i), DECADE_WIDTH * i, SCREEN_HEIGHT - 5);
+            pen.drawString("" + (STARTING_YEAR + 10 * i), DECADE_WIDTH * i, SCREEN_HEIGHT - 8);
         }
     }
 
-    private static void drawData(Scanner lineReader, Graphics pen, String inputName, String namePopularity, String nameMeaning) {
+    private static void drawData(Graphics pen, String inputName, String namePopularity, String nameMeaning) {
         // Draw values on the graph (based on name data).
 
         // Name meanings.
         pen.setColor(Color.BLACK);
-        pen.drawString(nameMeaning, 0, 15);
+        pen.drawString(nameMeaning, 0, 16);
 
         // Get bar color (based on gender).
         Color barColor;
@@ -89,22 +100,34 @@ public class BabyNames {
         }
         
         // Get and draw data.
-        String data2 = namePopularity.substring(inputName.length() + 3);  // 'inputName.length() + 3' is the space before the values.
-        lineReader = new Scanner(data2);
+        String data = namePopularity.substring(inputName.length() + 3);  // 'inputName.length() + 3' is the space before the values.
+        Scanner lineReader = new Scanner(data);
         int rank;
         double barHeight;
         int xPos;
         int yPos;
+        int count = 0;
 
         while (lineReader.hasNext()) {
             pen.setColor(barColor);
             rank = lineReader.nextInt();
-            barHeight = rank == 0 ? 0 : (SCREEN_HEIGHT - LEGEND_HEIGHT * 2) - (rank / 1000.0 * SCREEN_HEIGHT);
-            xPos = (i - 2) * 60;
-            yPos = rank == 1000 ? SCREEN_HEIGHT - LEGEND_HEIGHT : (SCREEN_HEIGHT - (int) barHeight) - LEGEND_HEIGHT;
+            xPos = count * 60;
+            if (rank == 0) {
+                yPos = SCREEN_HEIGHT - LEGEND_HEIGHT;
+            }
+            else {
+                yPos = LEGEND_HEIGHT + rank / 2;
+            }
+            if (rank == 0) {
+                barHeight = 0;
+            }
+            else {
+                barHeight = SCREEN_HEIGHT - LEGEND_HEIGHT - yPos;
+            }
             pen.fillRect(xPos, yPos, DECADE_WIDTH / 2, (int) barHeight);
             pen.setColor(Color.BLACK);
-            pen.drawString(Integer.toString(rank), xPos, yPos);
+            pen.drawString("" + rank, xPos, yPos);
+            count++;
         }
     }
 }
