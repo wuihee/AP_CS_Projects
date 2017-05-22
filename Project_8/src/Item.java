232f0e7bd4c7b352f1f0d1bf038f8b6664e69086
Wuihee <wuihee@gmail.com>
2017-05-22 09:34:07 -0700

    Updated Item class.

--- Project_8/src/Item.java
+++ Project_8/src/Item.java
@@ -1,2 +1,23 @@
 public class Item {
+    private static String name;
+    private static double price;
+    private static int bulkQuantity;
+    private static double bulkPrice;
+
+    Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    Item(String name, double price, int bulkQuantity, double bulkPrice) {
+        this.name = name;
+        this.price = price;
+        this.bulkQuantity = bulkQuantity;
+        this.bulkPrice = bulkPrice;
+    }
+
+    private static double priceFor(int quantity) {
+        // Returns the price for a given quantity of the item.
+        return 1.1;
+    }
 }
