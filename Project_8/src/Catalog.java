1e610b5c10314a737de4f93f09ba7a841873132e
wuihee <wuihee@gmail.com>
2017-05-22 21:14:49 -0700

    Updated Catalog class

--- Project_8/src/Catalog.java
+++ Project_8/src/Catalog.java
@@ -9,18 +9,22 @@ public class Catalog {
     }
 
     void add(Item item) {
+        // Adds an Item at the end of this list.
         catalogItems.add(item);
     }
 
     int size() {
+        // Returns the number of items in this list.
         return catalogItems.size();
     }
 
     Item get(int index) {
+        // Returns the Item with the given index (0-based).
         return catalogItems.get(index);
     }
 
     String getName() {
+        // Returns the name of this catalog.
         return name;
     }
 }
